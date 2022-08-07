84a240d44087a9e4ff75ebcb1e43186e3e6d7f97
Alexander Jensen <a.jensen@24bytes.de>
2022-08-07 10:00:21 +0200

    Try Livewire’s PR #4876 approach

--- app/Providers/LivewireServiceprovider.php
+++ app/Providers/LivewireServiceprovider.php
@@ -3,17 +3,51 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Str;
+use Illuminate\View\Compilers\BladeCompiler;
 use Livewire;
 
 class LivewireServiceProvider extends Livewire\LivewireServiceProvider
 {
+    /**
+     * Specify Blade directives that should never be overwritten.
+     *
+     * @var string[][]
+     */
+    protected $bladeDirectivesToRegisterIfMissing = [
+        'js' => [Livewire\LivewireBladeDirectives::class, 'js'],
+    ];
+
     protected function registerBladeDirectives()
     {
-        // Blade::directive('js', [Livewire\LivewireBladeDirectives::class, 'js']);
+        foreach ($this->bladeDirectivesToRegisterIfMissing as $name => $callable) {
+            $this->registerBladeDirectiveIfNotRegistered($name, $callable);
+        }
+
         Blade::directive('this', [Livewire\LivewireBladeDirectives::class, 'this']);
         Blade::directive('entangle', [Livewire\LivewireBladeDirectives::class, 'entangle']);
         Blade::directive('livewire', [Livewire\LivewireBladeDirectives::class, 'livewire']);
         Blade::directive('livewireStyles', [Livewire\LivewireBladeDirectives::class, 'livewireStyles']);
         Blade::directive('livewireScripts', [Livewire\LivewireBladeDirectives::class, 'livewireScripts']);
+    }
+
+    protected function registerBladeDirectiveIfNotRegistered(string $name, callable $callable)
+    {
+        if (! $this->bladeDirectiveAlreadyRegistered($name)) {
+            Blade::directive($name, $callable);
+        }
+    }
+
+    protected function bladeDirectiveAlreadyRegistered(string $name): bool
+    {
+        if (method_exists(BladeCompiler::class, Str::start($name, 'compile'))) {
+            return true;
+        }
+
+        if (array_key_exists($name, Blade::getCustomDirectives())) {
+            return true;
+        }
+
+        return false;
     }
 }
